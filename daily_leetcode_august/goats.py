7e0ea98f28a36ced74ffb785453c1a61be806458
TahaKhan8899 <taha.khan8899@gmail.com>
2020-09-08 15:31:34 -0400

    Add august questions

--- daily_leetcode_august/goats.py
+++ daily_leetcode_august/goats.py
@@ -6,12 +6,12 @@ class Solution:
 
         for i in range(0, len(words)):
             if words[i][0] in vowels:
-                finalSentence += words[i] + "ma" + ('a' * (i+1)) + " "
+                finalSentence += words[i] + "ma" + ('a' * (i + 1)) + " "
             else:
-                finalSentence += words[i][1:] + \
-                    words[i][0] + "ma" + ('a' * (i+1)) + " "
+                finalSentence += words[i][1:] + words[i][0] + \
+                    "ma" + ('a' * (i + 1)) + " "
 
-        return finalSentence[:len(finalSentence)-1]
+        return finalSentence
 
 
 obj = Solution()
--- daily_leetcode_august/test.py
+++ daily_leetcode_august/test.py
@@ -1,10 +1,14 @@
-def myFunction(nums):
-    sum = 0
-    for i in range(0, len(nums)):
-        sum += nums[i]
-    return sum
+def give_candies(num_candies, people):
+    count = 1
+    # this list copy is just to keep the function pure
+    li = [x for x in people]
+    while num_candies >= count:
+        print("Count: ", count, "index: ", (count-1) % len(li))
+        li[(count-1) % len(li)] += count
+        num_candies -= count
+        count += 1
+    li[(count-1) % len(li)] += num_candies
+    return li
 
 
-nums = [2, 2, 2, 2, 2]
-answer = myFunction(nums)
-print(answer)
+print(give_candies(7, [0 for i in range(4)]))
